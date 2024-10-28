e782930ebbf46115c4f991a161de1b5d96dc2cf2
Reagan Charana <rgncharana2@gmail.com>
2024-10-28 09:37:00 +0300

    chore:

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ open.addEventListener("click", async () => {
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
-      }
+      }''
 
       const data = await response.json();
       afterPromptResponse.innerText = data.response;
